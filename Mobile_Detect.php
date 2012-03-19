266baa95aa7dd7d0e35ece958ccbbfb4c61f7ed2
serbanghita@gmail.com <serbanghita@gmail.com@4c3b34b8-4606-11de-b695-a305669b6508>
2012-03-19 16:45:41 +0000

    trunk updated to 2.x sources
    git-svn-id: https://php-mobile-detect.googlecode.com/svn/trunk@24 4c3b34b8-4606-11de-b695-a305669b6508

--- Mobile_Detect.php
+++ Mobile_Detect.php
@@ -1,91 +1,239 @@
 <?php
-
 /**
  * Mobile Detect
+ * 
+ * @usage      require_once 'Mobile_Detect.php';
+ *             $detect = new Mobile_Detect();
+ *             $detect->isMobile() or $detect->isTablet()
+ * 
+ *             For more specific usage see the documentation inside the class.
+ *             $detect->isAndroidOS() or $detect->isiPhone() ...
+ * 
  * @license    http://www.opensource.org/licenses/mit-license.php The MIT License
  */
-class Mobile_Detect
-{
 
-	protected $accept;
-	protected $userAgent;
-	protected $isMobile = false;
-	protected $isAndroid = null;
-	protected $isAndroidtablet = null;
-	protected $isIphone = null;
-	protected $isIpad = null;
-	protected $isBlackberry = null;
-	protected $isBlackberrytablet = null;
-	protected $isPalm = null;
-	protected $isWindows = null;
-	protected $isWindowsphone = null;
-	protected $isGeneric = null;
-	protected $devices = array(
-		"android" => "android.*mobile",
-		"androidtablet" => "android(?!.*mobile)",
-		"blackberry" => "blackberry",
-		"blackberrytablet" => "rim tablet os",
-		"iphone" => "(iphone|ipod)",
-		"ipad" => "(ipad)",
-		"palm" => "(avantgo|blazer|elaine|hiptop|palm|plucker|xiino)",
-		"windows" => "windows ce; (iemobile|ppc|smartphone)",
-		"windowsphone" => "windows phone os",
-		"generic" => "(kindle|mobile|mmp|midp|pocket|psp|symbian|smartphone|treo|up.browser|up.link|vodafone|wap|opera mini|opera mobi)"
-	);
+class Mobile_Detect {
+    
+    protected $detectionRules;
+    protected $userAgent = null;
+    protected $accept = null;
+    // Assume the visitor has a desktop environment.
+    protected $isMobile = false;
+    protected $isTablet = false;
+    protected $phoneDeviceName = null;
+    protected $tabletDevicename = null;
+    protected $operatingSystemName = null;
+    protected $userAgentName = null;
+    // List of mobile devices (phones)
+    protected $phoneDevices = array(     
+            'iPhone' => '(iPhone.*Mobile|iPod|iTunes)',            
+            'BlackBerry' => 'BlackBerry|rim[0-9]+',
+            'HTC' => 'HTC|Desire',
+            'Nexus' => 'Nexus One|Nexus S',
+            'DellStreak' => 'Dell Streak',
+            'Motorola' => '\bDroid\b.*Build|HRI39|MOT\-',
+            'Samsung' => 'Samsung|GT-P1000|SGH-T959D',
+            'Sony' => 'E10i',
+            'Asus' => 'Asus.*Galaxy',
+            'Palm' => 'PalmSource|Palm', // avantgo|blazer|elaine|hiptop|plucker|xiino
+            'GenericPhone' => '(mmp|pocket|psp|symbian|Smartphone|smartfon|treo|up.browser|up.link|vodafone|wap|nokia|SonyEricsson|N900|\bPPC\b|MAUI.*WAP.*Browser)'
+    );
+    // List of tablet devices.
+    protected $tabletDevices = array(
+        'BlackBerryTablet' => 'PlayBook|RIM Tablet',
+        'iPad' => 'iPad.*Mobile',
+        'Kindle' => 'Kindle|Silk.*Accelerated',
+        'SamsungTablet' => 'SCH\-I800|GT\-P1000',
+        'MotorolaTablet' => 'xoom|sholest',
+        'AsusTablet' => 'Transformer|TF101',
+        'GenericTablet' => 'Tablet|ViewPad7|LG\-V909|MID7015|BNTV250A|LogicPD Zoom2|\bA7EB\b|CatNova8|A1_07|CT704|CT1002|\bM721\b',
+    );
+    // List of mobile Operating Systems.
+    protected $operatingSystems = array(
+        'AndroidOS' => '(android.*mobile|android(?!.*mobile))',
+        'BlackBerryOS' => '(blackberry|rim tablet os)',
+        'PalmOS' => '(avantgo|blazer|elaine|hiptop|palm|plucker|xiino)',
+        'SymbianOS' => 'Symbian',
+        'WindowsMobileOS' => 'IEMobile|Windows Phone|Windows CE.*(PPC|Smartphone)|MSIEMobile|Window Mobile|XBLWP7',
+        'iOS' => '(iphone|ipod|ipad)',
+        'FlashLiteOS' => '',
+        'JavaOS' => '',
+        'NokiaOS' => '',
+        'webOS' => '',
+        'badaOS' => '',
+        'BREWOS' => '',
+    );
+    // List of mobile User Agents.
+    protected $userAgents = array(      
+      'Chrome' => '\bCrMo\b',
+      'Dolfin' => 'Dolfin',
+      'Opera' => '(Opera Mini|Opera Mobi)',  
+      'Skyfire' => 'skyfire',      
+      'IE' => 'ie*mobile',
+      'Firefox' => 'fennec|firefox.*maemo',
+      'Bolt' => 'bolt',
+      'TeaShark' => 'teashark',
+      'Blazer' => 'Blazer',
+      'Safari' => 'Mobile*Safari',
+      'Midori' => 'midori',
+      'GenericBrowser' => 'NokiaBrowser'
+    );
+    
+    function __construct(){
+        
+        // Merge all rules together.
+        $this->detectionRules = array_merge(
+                                            $this->phoneDevices, 
+                                            $this->tabletDevices, 
+                                            $this->operatingSystems, 
+                                            $this->userAgents
+                                            );
+        $this->userAgent = $_SERVER['HTTP_USER_AGENT'];
+        $this->accept = $_SERVER['HTTP_ACCEPT'];  
+        
+        if (
+		isset($_SERVER['HTTP_X_WAP_PROFILE']) ||
+                isset($_SERVER['HTTP_X_WAP_CLIENTID']) ||
+                isset($_SERVER['HTTP_WAP_CONNECTION']) ||
+		isset($_SERVER['HTTP_PROFILE']) ||
+		isset($_SERVER['HTTP_X_OPERAMINI_PHONE_UA']) || // Reported by Nokia devices (eg. C3)
+                isset($_SERVER['HTTP_X_NOKIA_IPADDRESS']) ||
+                isset($_SERVER['HTTP_X_NOKIA_GATEWAY_ID']) ||
+                isset($_SERVER['HTTP_X_ORANGE_ID']) ||
+                isset($_SERVER['HTTP_X_VODAFONE_3GPDPCONTEXT']) ||
+                isset($_SERVER['HTTP_X_HUAWEI_USERID']) ||
+                isset($_SERVER['HTTP_UA_OS']) // Reported by Windows Smartphones
+		) {
+                $this->isMobile = true;
+        } elseif (!empty($this->accept) && (strpos($this->accept, 'text/vnd.wap.wml') !== false || strpos($this->accept, 'application/vnd.wap.xhtml+xml') !== false)) {
+                $this->isMobile = true;
+        } else {
+                $this->_detect();
+        }        
+        
+    }
+	
+    public function getRules()
+    {
+        return $this->detectionRules;
+    }
+    
+    /**
+     * 
+     * @method boolean isiPhone()
+     * @method boolean isBlackBerry()
+     * @method boolean isHTC()
+     * @method boolean isNexus()
+     * @method boolean isDellStreak()
+     * @method boolean isMotorola()
+     * @method boolean isSamsung()
+     * @method boolean isSony()
+     * @method boolean isAsus()
+     * @method boolean isPalm()
+     *
+     * @method boolean isBlackBerryTablet()
+     * @method boolean isiPad()
+     * @method boolean isKindle()
+     * @method boolean isSamsungTablet()
+     * @method boolean isMotorolaTablet()
+     * @method boolean isAsusTablet()
+     *       
+     * @method boolean isAndroidOS()
+     * @method boolean isBlackBerryOS()
+     * @method boolean isPalmOS()
+     * @method boolean isSymbianOS()
+     * @method boolean isWindowsMobileOS()
+     * @method boolean isiOS()
+     * 
+     * @method boolean isChrome()
+     * @method boolean isDolfin()
+     * @method boolean isOpera()
+     * @method boolean isSkyfire()
+     * @method boolean isIE()
+     * @method boolean isFirefox()
+     * @method boolean isBolt()
+     * @method boolean isTeaShark()
+     * @method boolean isBlazer()
+     * @method boolean isSafari()
+     * @method boolean isMidori()
+     *   
+     * @param string $name
+     * @param array $arguments
+     * @return mixed 
+     */
+    public function __call($name, $arguments)
+    {
+                
+        $key = substr($name, 2);
+        return $this->_detect($key);
+        
+    }
+	
+    private function _detect($key='')
+    {
 
-	public function __construct()
-	{
-		$this->userAgent = $_SERVER['HTTP_USER_AGENT'];
-		$this->accept = $_SERVER['HTTP_ACCEPT'];
+        if(empty($key)){ 
 
-		if (isset($_SERVER['HTTP_X_WAP_PROFILE']) || isset($_SERVER['HTTP_PROFILE'])) {
-			$this->isMobile = true;
-		} elseif (strpos($this->accept, 'text/vnd.wap.wml') > 0 || strpos($this->accept, 'application/vnd.wap.xhtml+xml') > 0) {
-			$this->isMobile = true;
-		} else {
-			foreach ($this->devices as $device => $regexp) {
-				if ($this->isDevice($device)) {
-					$this->isMobile = true;
-				}
-			}
-		}
-	}
+            // Begin general search.
+            foreach($this->detectionRules as $_key => $_regex){
+                if(empty($_regex)){ continue; }
+                if(preg_match('/'.$_regex.'/is', $this->userAgent)){
+                    $this->isMobile = true;
+                    var_dump($_regex);
+                    return true;
+                } 
+            }
+            return false;
 
-	/**
-	 * Overloads isAndroid() | isAndroidtablet() | isIphone() | isIpad() | isBlackberry() | isBlackberrytablet() | isPalm() | isWindowsphone() | isWindows() | isGeneric() through isDevice()
-	 *
-	 * @param string $name
-	 * @param array $arguments
-	 * @return bool
-	 */
-	public function __call($name, $arguments)
-	{
-		$device = substr($name, 2);
-		if ($name == "is" . ucfirst($device) && array_key_exists(strtolower($device), $this->devices)) {
-			return $this->isDevice($device);
-		} else {
-			trigger_error("Method $name not defined", E_USER_WARNING);
-		}
-	}
+        } else {
+            
+            // Search for a certain key.
+            // Make the keys lowecase so we can match: isIphone(), isiPhone(), isiphone(), etc.
+            $key = strtolower($key);
+            $_rules = array_change_key_case($this->detectionRules);
+            
+            if(array_key_exists($key, $_rules)){
+                if(empty($_rules[$key])){ return null; }
+                if(preg_match('/'.$_rules[$key].'/is', $this->userAgent)){
+                    $this->isMobile = true;
+                    return true;
+                } else {
+                    return false;
+                }           
+            } else {
+                trigger_error("Method $name is not defined", E_USER_WARNING);
+            }	
 
-	/**
-	 * Returns true if any type of mobile device detected, including special ones
-	 * @return bool
-	 */
-	public function isMobile()
-	{
-		return $this->isMobile;
-	}
+        }
 
-	protected function isDevice($device)
-	{
-		$var = "is" . ucfirst($device);
-		$return = $this->$var === null ? (bool) preg_match("/" . $this->devices[strtolower($device)] . "/i", $this->userAgent) : $this->$var;
-		if ($device != 'generic' && $return == true) {
-			$this->isGeneric = false;
-		}
-
-		return $return;
-	}
-
+    }
+        
+    /**
+    * Returns true if any type of mobile device detected, including special ones
+    * @return bool
+    */
+    public function isMobile()
+    {
+            return $this->isMobile;
+    } 
+    
+    /**
+     * Return true if any type of tablet device is detected.
+     * @return boolean 
+     */
+    public function isTablet()
+    {
+        
+        foreach($this->tabletDevices as $_key => $_regex){
+            if(preg_match('/'.$_regex.'/is', $this->userAgent)){
+                $this->isTablet = true;
+                return true;
+            }
+        }
+        
+        return false;        
+            
+    }
+    
+    
 }
--- demo.php
+++ demo.php
@@ -1,4 +1,39 @@
-<!DOCTYPE html>
+<?php
+error_reporting(E_ALL & ~E_NOTICE);
+ini_set('display_errors', true);
+
+function writeToFile($file, $content){
+  
+    if (!$handle = fopen($file, 'a')) {
+            echo "Cannot open file ($file)";
+            exit;
+    }
+
+    if (fwrite($handle, $content) === FALSE) {
+            echo "Cannot write to file ($file)";
+            exit;
+    }
+
+    fclose($handle);
+
+}
+if (!function_exists('apache_request_headers')) { 
+
+        function apache_request_headers() { 
+            foreach($_SERVER as $key=>$value) { 
+                if (substr($key,0,5)=="HTTP_") { 
+                    //$key=str_replace(" ","-",ucwords(strtolower(str_replace("_"," ",substr($key,5))))); 
+					$out[$key]=$value; 
+                } 
+            } 
+            return $out; 
+        } 
+ 
+} 
+
+writeToFile('ua.txt', print_r(apache_request_headers(),true));
+
+?><!DOCTYPE html>
 <html xmlns="http://www.w3.org/1999/xhtml" lang="en" xml:lang="en">
     <head>
         <meta charset="utf-8">
@@ -26,8 +61,11 @@
                 color: #005580;
                 text-decoration: underline;
             }  
-            .mobile { background-color:green; color:white; }
             .desktop { background-color:blue; color:white; } 
+            .tablet { background-color:yellow; color:black; }
+            .mobile,.true { background-color:green; color:white; }
+            .randomcrap { background:#eee; color:#666; }
+            
         </style>
     </head>
     <body>
@@ -39,11 +77,42 @@
             <?php
                 require_once 'Mobile_Detect.php';
                 $detect = new Mobile_Detect();
-                $layout = ($detect->isMobile() ? 'mobile' : 'desktop');
+                $layout = ($detect->isMobile() ? ($detect->isTablet() ? 'tablet' : 'mobile') : 'desktop');
             ?>            
             <p class="<?php echo $layout; ?>">This is <b><?php echo $layout; ?></b>. Your UA is <b><?php echo htmlentities($_SERVER['HTTP_USER_AGENT']); ?></b></p>
-            
-        </section>
+       </section>
+	   
+	   <section>
+		   <header>
+			   <h1>Tests</h1>
+		   </header>
+            <table>
+                <?php foreach($detect->getRules() as $name => $regex):
+                                $check = $detect->{'is'.$name}();
+                ?>
+                    <tr>
+                            <td>is<?php echo $name; ?>()</td>
+                            <td <?php if($check): ?>class="true"<?php endif; ?>><?php echo var_dump($check); ?></td>
+                    </tr>
+                <?php endforeach; ?>
+                    <tr>
+                        <td colspan="2"><hr></td>
+                    </tr>
+                    <tr>
+                            <td>isRandomCrap()</td>
+                            <td class="randomcrap"><?php echo var_dump($detect->isRandomCrap()); ?></td>
+                    </tr> 
+                    <tr>
+                            <td>isIphone()</td>
+                            <td><?php echo var_dump($detect->isIphone()); ?></td>
+                    </tr>  
+                    <tr>
+                            <td>istablet()</td>
+                            <td><?php echo var_dump($detect->istablet()); ?></td>
+                    </tr>                  
+            </table>
+       </section>     
+        
         
         <footer>
             <p>Copyright &copy; <?php echo date('Y'); ?></p>
