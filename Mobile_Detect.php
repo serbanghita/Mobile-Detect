d61be54ff82d3b783f22bcc02d9aa1db0f6ecaf6
serbanghita@gmail.com <serbanghita@gmail.com@4c3b34b8-4606-11de-b695-a305669b6508>
2012-04-24 19:10:16 +0000

    - HTC tablets support - Yarvik tablets support - Firefox and Safari mobile regex updates - removed comments, added meaningful comments
    git-svn-id: https://php-mobile-detect.googlecode.com/svn/trunk@41 4c3b34b8-4606-11de-b695-a305669b6508

--- Mobile_Detect.php
+++ Mobile_Detect.php
@@ -7,8 +7,8 @@
  *             $detect = new Mobile_Detect();
  *             $detect->isMobile() or $detect->isTablet()
  * 
- *             For more specific usage see the documentation inside the class.
- *             $detect->isAndroidOS() or $detect->isiPhone() ...
+ *             For more specific usage see the documentation navigate to:
+ *             http://code.google.com/p/php-mobile-detect/wiki/Mobile_Detect
  * 
  * @license    http://www.opensource.org/licenses/mit-license.php The MIT License
  */
@@ -29,7 +29,7 @@ class Mobile_Detect {
     protected $phoneDevices = array(     
             'iPhone' => '(iPhone.*Mobile|iPod|iTunes)',            
             'BlackBerry' => 'BlackBerry|rim[0-9]+',
-            'HTC' => 'HTC|Desire',
+            'HTC' => 'HTC|HTC.*(A7272|S510e|C110e|Legend|Desire|T8282)|APX515CKT',
             'Nexus' => 'Nexus One|Nexus S',
             'DellStreak' => 'Dell Streak',
             'Motorola' => '\bDroid\b.*Build|HRI39|MOT\-',
@@ -45,10 +45,12 @@ class Mobile_Detect {
         'iPad' => 'iPad.*Mobile',
         'Kindle' => 'Kindle|Silk.*Accelerated',
         'SamsungTablet' => 'SAMSUNG.*Tablet|Galaxy.*Tab|GT-P1000|GT-P1010|GT-P6210|GT-P6800|GT-P6810|GT-P7100|GT-P7300|GT-P7310|GT-P7500|GT-P7510|SCH-I800|SCH-I815|SCH-I905|SGH-I777|SGH-I957|SGH-I987|SGH-T849|SGH-T859|SGH-T869|SGH-T989|SPH-D710|SPH-P100',
-        'MotorolaTablet' => 'xoom|sholest',
+        'HTCtablet' => 'HTC Flyer|HTC Jetstream|HTC-P715a|HTC EVO View 4G|PG41200',
+	'MotorolaTablet' => 'xoom|sholest',
         'AsusTablet' => 'Transformer|TF101',
         'NookTablet' => 'NookColor|nook browser|BNTV250A|LogicPD Zoom2',
         'AcerTablet' => 'Android.*(A100|A101|A200|A500|A501|A510|W500|W500P|W501|W501P)',
+	'YarvikTablet' => 'Android.*(TAB210|TAB211|TAB224|TAB250|TAB260|TAB264|TAB310|TAB360|TAB364|TAB410|TAB411|TAB420|TAB424|TAB450|TAB460|TAB461|TAB464|TAB465|TAB467|TAB468)',
         'GenericTablet' => 'Tablet|ViewPad7|LG\-V909|MID7015|BNTV250A|LogicPD Zoom2|\bA7EB\b|CatNova8|A1_07|CT704|CT1002|\bM721\b',
     );
     // List of mobile Operating Systems.
@@ -70,16 +72,16 @@ class Mobile_Detect {
     protected $userAgents = array(      
       'Chrome' => '\bCrMo\b|Chrome\/[.0-9]* Mobile',
       'Dolfin' => '\bDolfin\b',
-      'Opera' => '(Opera.*Mini|Opera.*Mobi)',  
+      'Opera' => 'Opera.*Mini|Opera.*Mobi',  
       'Skyfire' => 'skyfire',      
-      'IE' => 'ie*mobile',
-      'Firefox' => 'fennec|firefox.*maemo',
+      'IE' => 'IEMobile',
+      'Firefox' => 'fennec|firefox.*maemo|(Mobile|Tablet).*Firefox|Firefox.*Mobile',
       'Bolt' => 'bolt',
       'TeaShark' => 'teashark',
       'Blazer' => 'Blazer',
-      'Safari' => 'Mobile*Safari',
+      'Safari' => 'Mobile.*Safari|Safari.*Mobile',
       'Midori' => 'midori',
-      'GenericBrowser' => 'NokiaBrowser|OviBrowser'
+      'GenericBrowser' => 'NokiaBrowser|OviBrowser|SEMC.*Browser'
     );
     
     function __construct(){
@@ -123,44 +125,9 @@ class Mobile_Detect {
     }
     
     /**
+     * Magic overloading method.
      * 
-     * @method boolean isiPhone()
-     * @method boolean isBlackBerry()
-     * @method boolean isHTC()
-     * @method boolean isNexus()
-     * @method boolean isDellStreak()
-     * @method boolean isMotorola()
-     * @method boolean isSamsung()
-     * @method boolean isSony()
-     * @method boolean isAsus()
-     * @method boolean isPalm()
-     *
-     * @method boolean isBlackBerryTablet()
-     * @method boolean isiPad()
-     * @method boolean isKindle()
-     * @method boolean isSamsungTablet()
-     * @method boolean isMotorolaTablet()
-     * @method boolean isAsusTablet()
-     *       
-     * @method boolean isAndroidOS()
-     * @method boolean isBlackBerryOS()
-     * @method boolean isPalmOS()
-     * @method boolean isSymbianOS()
-     * @method boolean isWindowsMobileOS()
-     * @method boolean isiOS()
-     * 
-     * @method boolean isChrome()
-     * @method boolean isDolfin()
-     * @method boolean isOpera()
-     * @method boolean isSkyfire()
-     * @method boolean isIE()
-     * @method boolean isFirefox()
-     * @method boolean isBolt()
-     * @method boolean isTeaShark()
-     * @method boolean isBlazer()
-     * @method boolean isSafari()
-     * @method boolean isMidori()
-     *   
+     * @method boolean is[...]()
      * @param string $name
      * @param array $arguments
      * @return mixed 
@@ -172,14 +139,21 @@ class Mobile_Detect {
         return $this->_detect($key);
         
     }
-	
+    
+    /**
+     * Private method that does the detection of the 
+     * mobile devices.
+     * 
+     * @param type $key
+     * @return boolean|null 
+     */
     private function _detect($key='')
     {
 
         if(empty($key)){ 
 
             // Begin general search.
-            foreach($this->detectionRules as $_key => $_regex){
+            foreach($this->detectionRules as $_regex){
                 if(empty($_regex)){ continue; }
                 if(preg_match('/'.$_regex.'/is', $this->userAgent)){
                     $this->isMobile = true;
@@ -214,30 +188,32 @@ class Mobile_Detect {
     }
         
     /**
+    * Check if the device is mobile.
     * Returns true if any type of mobile device detected, including special ones
     * @return bool
     */
     public function isMobile()
     {
-            return $this->isMobile;
+	    return $this->isMobile;
     } 
     
     /**
-     * Return true if any type of tablet device is detected.
-     * @return boolean 
-     */
+    * Check if the device is a tablet.
+    * Return true if any type of tablet device is detected.
+    * @return boolean 
+    */
     public function isTablet()
     {
-        
-        foreach($this->tabletDevices as $_key => $_regex){
-            if(preg_match('/'.$_regex.'/is', $this->userAgent)){
-                $this->isTablet = true;
-                return true;
-            }
-        }
-        
-        return false;        
-            
+
+	foreach($this->tabletDevices as $_regex){
+	    if(preg_match('/'.$_regex.'/is', $this->userAgent)){
+		$this->isTablet = true;
+		return true;
+	    }
+	}
+
+	return false;        
+
     }
     
     
