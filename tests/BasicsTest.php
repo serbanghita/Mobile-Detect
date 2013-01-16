0c66270a0eaab3beabcf1ce22b20e3610cbe7df6
Serban Ghita <serbanghita@gmail.com>
2013-01-16 13:18:22 +0200

    Test fix.

--- tests/BasicsTest.php
+++ tests/BasicsTest.php
@@ -3,6 +3,7 @@ class BasicTest extends PHPUnit_Framework_TestCase {
 
 	public function testClassExists(){
 
+		$detect = new Mobile_Detect;
 		$this->assertTrue(class_exists('Mobile_Detect'));
 
 	}
