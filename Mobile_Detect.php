4f8bd57bab35667eed877b00c63df2121db7617b
Serban Ghita <serbanghita@gmail.com>
2013-03-11 16:39:41 +0200

    Putting @version back in the comments #56

--- Mobile_Detect.php
+++ Mobile_Detect.php
@@ -29,6 +29,7 @@
  * @link        Official page: http://mobiledetect.net
  *              GitHub Repository: https://github.com/serbanghita/Mobile-Detect
  *              Google Code Old Page: http://code.google.com/p/php-mobile-detect/
+ * @version     2.5.7
  */
 
 class Mobile_Detect {
