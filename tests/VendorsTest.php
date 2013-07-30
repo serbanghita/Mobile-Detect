966432f0fa5ed757d28155c8ad2ee8270adfd1fb
Nick Ilyin <nick.ilyin@gmail.com>
2013-07-30 07:57:45 -0400

    STYLE - fix VendorsTest

--- tests/VendorsTest.php
+++ tests/VendorsTest.php
@@ -27,91 +27,88 @@
  * @license     MIT License https://github.com/serbanghita/Mobile-Detect/blob/master/LICENSE.txt
  * @link        http://mobiledetect.net
  */
-class VendorsTest extends PHPUnit_Framework_TestCase {
+class VendorsTest extends PHPUnit_Framework_TestCase
+{
+    protected $detect;
+    protected static $items;
 
-	protected $detect;
-	protected static $items;
+    public function setUp()
+    {
+        $this->detect = new Mobile_Detect;
 
-	public function setUp(){
+    }
 
-		$this->detect = new Mobile_Detect;
-
-	}
-
-	public static function setUpBeforeClass()
-	{
-	    //this method could be called multiple times
-	    if (!self::$items) {
-		    self::$items = include dirname(__FILE__).'/UA_List.inc.php';
+    public static function setUpBeforeClass()
+    {
+        //this method could be called multiple times
+        if (!self::$items) {
+            self::$items = include dirname(__FILE__).'/UA_List.inc.php';
         }
-	}
+    }
 
-	public function testisMobileIsTablet(){
+    public function testisMobileIsTablet()
+    {
+        foreach (self::$items as $brand => $deviceArr) {
 
-		foreach(self::$items as $brand => $deviceArr){
+            foreach ($deviceArr as $userAgent => $conditions) {
 
-		    foreach($deviceArr as $userAgent => $conditions){
+                if (!is_array($conditions)) { continue; }
 
-		    	if(!is_array($conditions)){ continue; }
+                $this->detect->setUserAgent($userAgent);
 
-		    	$this->detect->setUserAgent($userAgent);
+                foreach ($conditions as $condition => $assert) {
 
-		    	foreach($conditions as $condition => $assert){
+                    // Currently not supporting version and model here.
+                    // @todo: I need to split this tests!
+                    if ( in_array($condition, array('model') ) ) { continue; } // 'version',
 
-		    		// Currently not supporting version and model here.
-		    		// @todo: I need to split this tests!
-		    		if( in_array($condition, array('model') ) ){ continue; } // 'version',
+                    switch ($condition) {
 
-		    		switch( $condition ){
+                        case 'version':
+                            // Android, iOS, Chrome, Build, etc.
+                            foreach ($assert as $assertKey => $assertValue) {
+                                //if ($brand == 'Apple') {
+                                //	echo 'UA ('.$condition.'('.$assertKey.') === '.$assertValue.'): '.$userAgent . "\n";
+                                //}
+                                $this->assertTrue( $this->detect->$condition( $assertKey ) == $assertValue, 'UA ('.$condition.'('.$assertKey.') === '.$assertValue.'): '.$userAgent);
+                            }
 
-		    			case 'version':
-		    				// Android, iOS, Chrome, Build, etc.
-		    				foreach( $assert as $assertKey => $assertValue){
-		    					//if( $brand == 'Apple'){
-		    					//	echo 'UA ('.$condition.'('.$assertKey.') === '.$assertValue.'): '.$userAgent . "\n";
-		    					//}
-		    					$this->assertTrue( $this->detect->$condition( $assertKey ) == $assertValue, 'UA ('.$condition.'('.$assertKey.') === '.$assertValue.'): '.$userAgent);
-		    				}
+                        break;
 
-		    			break;
+                        default:
+                            $this->assertTrue( $this->detect->$condition() === $assert, 'UA ('.$condition.'): '.$userAgent);
+                        break;
 
-		    			default:
-		    				$this->assertTrue( $this->detect->$condition() === $assert, 'UA ('.$condition.'): '.$userAgent);
-		    			break;
+                    }
 
-		    		}
+                }
 
+            }
 
-		    	}
+        }
 
+    }
 
-		    }
+    public function testVersion()
+    {
+        foreach (self::$items as $brand => $deviceArr) {
 
-		}
+            foreach ($deviceArr as $userAgent => $conditions) {
 
-	}
+                if ( !is_array($conditions) || !isset($conditions['version']) ) { continue; }
 
-	public function testVersion(){
+                $this->detect->setUserAgent($userAgent);
 
-		foreach( self::$items as $brand => $deviceArr ){
+                foreach ($conditions['version'] as $condition => $assertion) {
 
-			foreach( $deviceArr as $userAgent => $conditions ){
+                    $this->assertEquals( $this->detect->version($condition), $assertion, 'UA (version("'.$condition.'")): '.$userAgent );
 
-				if( !is_array($conditions) || !isset($conditions['version']) ){ continue; }
+                }
 
-				$this->detect->setUserAgent($userAgent);
+            }
 
-				foreach( $conditions['version'] as $condition => $assertion ){
+        }
 
-					$this->assertEquals( $this->detect->version($condition), $assertion, 'UA (version("'.$condition.'")): '.$userAgent );
-
-				}
-
-			}
-
-		}
-
-	}
-
+    }
 
 }
