a18d697052381184be083ec60d2fce476603da35
Lars Moelleken <lmoelleken@menadwork.com>
2016-01-31 15:29:05 +0100

    [*]: convert tabs -> spaces

--- export/exportToJSON.php
+++ export/exportToJSON.php
@@ -17,54 +17,54 @@
 
 // Included nicejson function to beautify the result JSON file.
 // This library is not mandatory.
-if( file_exists(dirname(__FILE__).'/nicejson/nicejson.php') ) {
-	include_once dirname(__FILE__).'/nicejson/nicejson.php';
+if (file_exists(dirname(__FILE__) . '/nicejson/nicejson.php')) {
+  include_once dirname(__FILE__) . '/nicejson/nicejson.php';
 }
 
 // Include Mobile Detect.
-require_once dirname(__FILE__).'/../Mobile_Detect.php';
+require_once dirname(__FILE__) . '/../Mobile_Detect.php';
 $detect = new Mobile_Detect;
 
 $json = array(
-				// The current version of Mobile Detect class that
-				// is being exported.
-				'version' => $detect->getScriptVersion(),
+    // The current version of Mobile Detect class that
+    // is being exported.
+    'version'       => $detect->getScriptVersion(),
 
-				// All headers that trigger 'isMobile' to be 'true',
-				// before reaching the User-Agent match detection.
-				'headerMatch' => $detect->getMobileHeaders(),
+    // All headers that trigger 'isMobile' to be 'true',
+    // before reaching the User-Agent match detection.
+    'headerMatch'   => $detect->getMobileHeaders(),
 
-				// All possible User-Agent headers.
-				'uaHttpHeaders' => $detect->getUaHttpHeaders(),
+    // All possible User-Agent headers.
+    'uaHttpHeaders' => $detect->getUaHttpHeaders(),
 
-				// All the regexes that trigger 'isMobile' or 'isTablet'
-				// to be true.
-				'uaMatch' => array(
-					// If match is found, triggers 'isMobile' to be true.
-					'phones'   => $detect->getPhoneDevices(),
-					// Triggers 'isTablet' to be true.
-					'tablets'  => $detect->getTabletDevices(),
-					// If match is found, triggers 'isMobile' to be true.
-					'browsers' => $detect->getBrowsers(),
-					// If match is found, triggers 'isMobile' to be true.
-					'os'       => $detect->getOperatingSystems(),
-					// Various utilities. To be further discussed.
-					'utilities' => $detect->getUtilities()
-				)
+    // All the regexes that trigger 'isMobile' or 'isTablet'
+    // to be true.
+    'uaMatch'       => array(
+        // If match is found, triggers 'isMobile' to be true.
+        'phones'    => $detect->getPhoneDevices(),
+        // Triggers 'isTablet' to be true.
+        'tablets'   => $detect->getTabletDevices(),
+        // If match is found, triggers 'isMobile' to be true.
+        'browsers'  => $detect->getBrowsers(),
+        // If match is found, triggers 'isMobile' to be true.
+        'os'        => $detect->getOperatingSystems(),
+        // Various utilities. To be further discussed.
+        'utilities' => $detect->getUtilities(),
+    ),
 
-			);
+);
 
 $jsonString = function_exists('json_format') ? json_format($json) : json_encode($json);
 
 // Write the JSON file to disk.
 // You can import this file in your app.
-$fileName = dirname(__FILE__).'/../Mobile_Detect.json';
+$fileName = dirname(__FILE__) . '/../Mobile_Detect.json';
 $handle = fopen($fileName, 'w');
 $fwrite = fwrite($handle, $jsonString);
 fclose($handle);
 
-if($fwrite){
-	echo 'Done. Check '.realpath($fileName).' file.';
+if ($fwrite) {
+    echo 'Done. Check ' . realpath($fileName) . ' file.';
 } else {
-	echo 'Failed to write '.realpath($fileName).' to disk.';
+    echo 'Failed to write ' . realpath($fileName) . ' to disk.';
 }
