d772569880e7becaec564274a3b93501b3fde700
Serban Ghita <sghita@adobe.com>
2016-03-24 11:06:37 +0200

    Version update.

--- Mobile_Detect.php
+++ Mobile_Detect.php
@@ -22,7 +22,7 @@
  *              README:       https://github.com/serbanghita/Mobile-Detect/blob/master/README.md
  *              HOWTO:        https://github.com/serbanghita/Mobile-Detect/wiki/Code-examples
  *
- * @version     2.8.19
+ * @version     2.8.20
  */
 
 class Mobile_Detect
@@ -66,7 +66,7 @@ class Mobile_Detect
     /**
      * Stores the version number of the current release.
      */
-    const VERSION                   = '2.8.19';
+    const VERSION                   = '2.8.20';
 
     /**
      * A type for the version() method indicating a string return value.
