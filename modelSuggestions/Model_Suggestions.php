e19f0d01519f8474138f3855443e84f15ce3d7c3
LeeJiunYih <jackjiunyihlee@gmail.com>
2020-08-20 01:15:47 +0800

    adding new section: Model_Suggestions

---
+++ modelSuggestions/Model_Suggestions.php
@@ -0,0 +1,118 @@
+<?php 
+/**
+*   Model_Suggestion
+*   Template / Format:
+*       Model Name:
+*       Model Brand:
+*       Model Version:
+*       OS:
+*       isTablet:
+*       isMobile:
+*       Model detail source: //suggestion: https://www.gsmarena.com/
+*       updated in script: Yes / No 
+*
+*   #001
+*       Model Name:     Samsung Galaxy Fold
+*       Model Brand:    Samsung
+*       Model Version:  SM-F900F
+*       OS:             AndroidOS
+*       isTablet:       True
+*       isMobile:       True
+*       Source:         https://www.gsmarena.com/samsung_galaxy_fold-9523.php
+*       updated:        No 
+*
+*   #002
+*       Model Name:     Samsung Galaxy S8
+*       Model Brand:    Samsung
+*       Model Version:  G950F
+*       OS:             AndroidOS
+*       isTablet:       False
+*       isMobile:       True
+*       Source:         https://www.gsmarena.com/samsung_galaxy_s8-8161.php
+*       updated:        No 
+*
+*   #003
+*       Model Name:     Samsung Galaxy S8+
+*       Model Brand:    Samsung
+*       Model Version:  G955F
+*       OS:             AndroidOS
+*       isTablet:       False
+*       isMobile:       True
+*       Source:         https://www.gsmarena.com/samsung_galaxy_s8+-8523.php
+*       updated:        No 
+*
+*   #004
+*       Model Name:     Samsung Galaxy S9
+*       Model Brand:    Samsung
+*       Model Version:  G960F
+*       OS:             AndroidOS
+*       isTablet:       False
+*       isMobile:       True
+*       Source:         https://www.gsmarena.com/samsung_galaxy_s9-8966.php
+*       updated:        No 
+*
+*   #005
+*       Model Name:     Samsung Galaxy S9+
+*       Model Brand:    Samsung
+*       Model Version:  G965F
+*       OS:             AndroidOS
+*       isTablet:       False
+*       isMobile:       True
+*       Source:         https://www.gsmarena.com/samsung_galaxy_s9+-8967.php
+*       updated:        No 
+*
+*   #006
+*       Model Name:     Samsung Galaxy S10
+*       Model Brand:    Samsung
+*       Model Version:  SM-G973F/DS
+*       OS:             AndroidOS
+*       isTablet:       False
+*       isMobile:       True
+*       Source:         https://www.gsmarena.com/samsung_galaxy_s10-9536.php
+*       updated:        No 
+*
+*   #007
+*       Model Name:     Samsung Galaxy S10+
+*       Model Brand:    Samsung
+*       Model Version:  SM-G975F/DS
+*       OS:             AndroidOS
+*       isTablet:       False
+*       isMobile:       True
+*       Source:         https://www.gsmarena.com/samsung_galaxy_s10+-9535.php
+*       updated:        No 
+*
+*   #008
+*       Model Name:     Samsung Galaxy A51
+*       Model Brand:    Samsung
+*       Model Version:  SM-A515F/DSN
+*       OS:             AndroidOS
+*       isTablet:       False
+*       isMobile:       True
+*       Source:         https://www.gsmarena.com/samsung_galaxy_a51-9963.php
+*       updated:        No 
+*
+*   #009
+*       Model Name:     Samsung Galaxy M31
+*       Model Brand:    Samsung
+*       Model Version:  SM-M315F/DS
+*       OS:             AndroidOS
+*       isTablet:       False
+*       isMobile:       True
+*       Source:         https://www.gsmarena.com/samsung_galaxy_m31-10079.php
+*       updated:        No 
+*
+*   #010
+*       Model Name:     Samsung Galaxy A31
+*       Model Brand:    Samsung
+*       Model Version:  SM-A315F/DS
+*       OS:             AndroidOS
+*       isTablet:       False
+*       isMobile:       True
+*       Source:         https://www.gsmarena.com/samsung_galaxy_a31-10149.php
+*       updated:        No 
+*
+*   #011
+
+* */
+
+?>
