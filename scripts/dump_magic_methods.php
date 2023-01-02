b54027c2471fa8309d2ca02446aec7d40f262df0
Sebastian Blank <blank@data-factory.net>
2023-01-02 16:41:04 +0100

    Fix: Method annotations are in the wrong place

--- scripts/dump_magic_methods.php
+++ scripts/dump_magic_methods.php
@@ -2,12 +2,14 @@
 
 use Detection\MobileDetect;
 
-$detect = new MobileDetect;
+require_once __DIR__ . '/../src/MobileDetect.php';
+
+$detect = new MobileDetect();
 
 /**
  * Dump all methods (+ extended)
  * Use this script to generate comments like "@method bool isiPhone()"
- * php -a examples/dump_magic_methods.php > methods.txt
+ * php export/dump_magic_methods.php > methods.txt
  */
 foreach ($detect->getRules() as $name => $regex) {
     echo "is$name()\n";
