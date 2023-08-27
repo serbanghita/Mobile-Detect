3a99635d4f71b532d74ae0a39d8218960b45483c
Serban Ghita <serbanghita@gmail.com>
2023-08-27 21:37:33 +0300

    Autoload classmap

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,3 +1,7 @@
 <?php
 
 require_once dirname(__FILE__) . '/../src/MobileDetect.php';
+require_once dirname(__FILE__) . '/../src/Cache/CacheFactory.php';
+require_once dirname(__FILE__) . '/../src/Cache/CacheItem.php';
+require_once dirname(__FILE__) . '/../src/Cache/CacheItemPool.php';
+require_once dirname(__FILE__) . '/../src/Exception/MobileDetectException.php';
