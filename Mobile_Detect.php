7a0734a321e4af9d4752f8a5cce1b75af1fbc29a
Serban Ghita <serbanghita@gmail.com>
2026-04-14 12:03:36 +0300

    Version bump to 2.8.46

--- Mobile_Detect.php
+++ Mobile_Detect.php
@@ -18,7 +18,7 @@
  * @author  Nick Ilyin <nick.ilyin@gmail.com>
  * @author: Victor Stanciu <vic.stanciu@gmail.com> (original author)
  *
- * @version 2.8.45
+ * @version 2.8.46
  *
  * Auto-generated isXXXX() magic methods.
  * php -a examples/dump_magic_methods.php
@@ -255,7 +255,7 @@ class Mobile_Detect
     /**
      * Stores the version number of the current release.
      */
-    const VERSION                   = '2.8.45';
+    const VERSION                   = '2.8.46';
 
     /**
      * A type for the version() method indicating a string return value.
